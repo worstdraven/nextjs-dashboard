6cbb29f0844d5c9f6b6defc3b4bde84a50a31f30
sunpot <kadennolan9@gmail.com>
2025-11-14 22:04:01 +0900

    feat: add lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,6 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
